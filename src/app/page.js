fca8d077be5f71fc6c8130fb49280f4ad5aa23a3
Shawal Islam <shawalislam606@gmail.com>
2025-01-30 01:43:46 +0600

    Added 404 page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import HomePage from "@/components/home/HomePage";
 export default function Home() {
   return (
     <main className="px-2 md:px-5 min-h-[calc(100vh-132px)] md:min-h-[calc(100vh-68px)] pt-[66px] md:pt-[90px]">
-      <HomePage />
+      <HomePage />  
     </main>
   );
 }
